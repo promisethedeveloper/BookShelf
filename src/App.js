a9d565c637361e833efae6f904aaae0b52da55d6
Promise Morka <76967971+promisethedeveloper@users.noreply.github.com>
2021-01-11 05:34:07 -0500

    Stored the author and title in variables to avoid hard coding them

--- src/App.js
+++ src/App.js
@@ -5,42 +5,23 @@ export default function App() {
   return (
     <article className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </article>
   );
 }
 
+const author = "Mario Puzo";
 const Book = () => {
+  const title = "The GodFather";
   return (
     <section className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/81QJnMbMQAL._AC_UY436_FMwebp_QL65_.jpg"
+        alt=""
+      />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
     </section>
   );
 };
 
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/81QJnMbMQAL._AC_UY436_FMwebp_QL65_.jpg"
-      alt=""
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>The GodFather</h1>;
-};
-const Author = () => {
-  return (
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-      Mario puzo
-    </h4>
-  );
-};
 // style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }
